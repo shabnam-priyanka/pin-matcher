18efd1700291bf9833618aff09790d6be2453090
shabnam-priyanka <66578322+shabnam-priyanka@users.noreply.github.com>
2020-08-03 14:11:06 -0400

    second commit

--- script.js
+++ script.js
@@ -4,50 +4,30 @@ function insert(num) {
 function empty() {
     document.getElementById("display").value = "";
 }
-// need to create 4 digit pin 
-function generatePin() {
-        const pin = Math.floor(1000 + Math.random() * 9000);
-        document.getElementById('display-pg').value = pin;
-    }
-// submit button 
-function submitPin(){
-     if(document.getElementById('display-pg').value == document.getElementById('display').value){
-            document.getElementById("pin-match").style.display = "block";
-            document.getElementById("not-matched").style.display = "none";
-        } else {
-            document.getElementById("not-matched").style.display = "block";
-            document.getElementById("pin-match").style.display = "none";
-        }            
-    }
 
-// Pin generator
-// function generatePin(){
-//     const pin = Math.floor(1000 + Math.random() * 9000);
-//     document.getElementById('generatorDisplay').value = pin;
-// }
-// // submit 
-// function submitPin(){
-//     if(document.getElementById('inputDisplay').value == document.getElementById('generatorDisplay').value){
-//         document.getElementById('matched').style.display = "block";
-//         document.getElementById("notMatched").style.display = "none";
-//     } else {
-//         document.getElementById("notMatched").style.display = "block";
-//         document.getElementById('matched').style.display = "none";
-//     }            
-// }
-// // Input number function
-// function input(num){
-//     document.getElementById('inputDisplay').value = document.getElementById('inputDisplay').value + num;
-// }
-// // Clear 
-// function clean(){
-//     document.getElementById('inputDisplay').value = '';
-// }
-// // backspace 
-// function back(){
-//     let value = document.getElementById('inputDisplay').value;
-//     document.getElementById('inputDisplay').value = value.substr(0, value.length -1);          
-// }
+
+// pin generator  
+function generatePin() {
+    const pin = Math.floor(1000 + Math.random() * 9000);
+    document.getElementById('display-pg').value = pin;
+}
+
+// submit button 
+function submitButton(){
+ if(document.getElementById('display-pg').value == document.getElementById('display').value){
+        document.getElementById("pin-match").style.display = "block";
+        document.getElementById("not-matched").style.display = "none";
+    } else {
+        document.getElementById("not-matched").style.display = "block";
+        document.getElementById("pin-match").style.display = "none";
+    }            
+}
+
+
+
+
+
+
 
 
 

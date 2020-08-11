4a5f737a37447cae26ffb351e446316fc440be39
shabnam-priyanka <66578322+shabnam-priyanka@users.noreply.github.com>
2020-08-10 23:35:07 -0400

    done

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+function display(num){
+    document.getElementById('display').value = document.getElementById('display').value + num;
 
-function insert(num) {
-    document.getElementById("display").value = document.getElementById("display").value + num;
-}
-function empty() {
-    document.getElementById("display").value = "";
 }
 
-
-// pin generator  
-function generatePin() {
-    const pin = Math.floor(1000 + Math.random() * 9000);
-    document.getElementById('display-pg').value = pin;
+function backSpace(){
+     document.value.substring(0, myTextarea.value.length - 1);
 }
 
-// submit button 
-function submitButton(){
- if(document.getElementById('display-pg').value == document.getElementById('display').value){
-        document.getElementById("wrong-pin").style.display = "block";
-        document.getElementById("pin-matched").style.display = "none";
-    } else {
-        document.getElementById("wrong-pin").style.display = "block";
-        document.getElementById("pin-matched").style.display = "none";
-    }            
+function erase(){
+    document.getElementById('display').value = "";
 }
 
+function generatePin(){
+    document.getElementById('display-pg').value = Math.floor(1000 + Math.random() * 9000);
 
+}
 
-
+function submitBtn(){
+    if ( document.getElementById('display').value == document.getElementById('display-pg').value) {
+        document.getElementById('pin-matched').style.display = 'block';
+        document.getElementById('wrong-pin').style.display = 'none';
+    }
+    else{
+        document.getElementById('wrong-pin').style.display = 'block';
+        document.getElementById('pin-matched').style.display = 'none';
+    }
+}
 
 
 
